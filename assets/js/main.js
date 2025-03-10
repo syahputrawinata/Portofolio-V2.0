d4593c4759fcfb0cd409a3179517822a30ff3a53
syahputrawinata <sputrawinata23@gmail.com>
2025-03-10 10:57:21 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,7 +38,33 @@ const shadowHeader = () =>{
 window.addEventListener('scroll', shadowHeader)
 
 /*=============== EMAIL JS===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message');
 
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_6tgggsq', 'template_1jvkbxs', '#contact-form', '6vTcfE43xbQ2Bl8Xz')
+    .then(() => {
+        // Show sent message
+        contactMessage.textContent = "Message sent successfully ✅";
+
+        // Remove message after five seconds
+        setTimeout(() => {
+            contactMessage.textContent = "";
+        }, 5000);
+
+        // Clear input fields
+        contactForm.reset();
+
+    }, () => {
+        // Show error message
+        contactMessage.textContent = "Message not sent (service error) ❌";
+    });
+}
+
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
